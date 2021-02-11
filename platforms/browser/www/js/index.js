2028319e29cd9490731939a7d9cf6ab5db1274c4
Dorian <doriandev2020@gmail.com>
2021-02-10 20:15:06 -0600

    implemented the cordova plugin to allow a picture to be taken on the users device

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -31,9 +31,22 @@ var app = {
 
     // Take a picture!
     takePhoto: function(){
-        console.log("Button Clicked!")
-    }
-};
+        console.log("Button Clicked!");
+        navigator.camera.getPicture(app.onSuccess, app.onFail, { quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL
+         })
+    },
+        
+         onSuccess: function(imageData) {
+            var image = document.getElementById('photo');
+            image.src = "data:image/jpeg;base64," + imageData;
+        },
+        
+         onFail: function(message) {
+            alert('Failed because: ' + message);
+        }
+    };
+
 
 app.initialize();
 
